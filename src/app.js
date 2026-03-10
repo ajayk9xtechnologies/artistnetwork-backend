cb38b30620553a149ee6ad9c716ccb5cd4c55717
ajuajay11 <ajay.k@9xtechnology.com>
2026-03-10 11:57:25 +0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 
 const cors = require("cors");
 const corsOptions = {
-  origin: ["http://localhost:5173","http://localhost:5173/", "http://localhost:5173/register"],
+  origin: ["http://localhost:5173", "http://localhost:3000", "http://localhost:3000/", "http://localhost:5173/", "http://localhost:5173/register"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
@@ -21,7 +21,7 @@ app.use(passport.initialize());
 // app.use(passport.session());
 
 app.use(express.json());
-app.use(cookieParser()); 
+app.use(cookieParser());
 // ← ADD THIS LINE before routes 
 // testing route
 app.get("/", (req, res) => {
